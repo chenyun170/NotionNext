fb0b0f7a5a35eb537b6ab458a32fe6b10955d3c3
chenyun170 <104297453+chenyun170@users.noreply.github.com>
2025-08-20 16:02:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'cy', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '分享外贸类信息', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.jx0791.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.123170.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'cy', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://a.clya.me/file/1744608564846_微信截图_20241220160311.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX

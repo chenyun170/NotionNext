af333ad5d5a6ca7bb462d713be0cfee5c6c746cd
chenyun170 <104297453+chenyun170@users.noreply.github.com>
2026-02-10 19:07:08 +0800

    Update page.processor.ts

--- lib/site/processors/page.processor.ts
+++ lib/site/processors/page.processor.ts
@@ -1,10 +1,21 @@
-import { isInRange } from '@/lib/utils/time.util'
-import type { SiteData } from '../site.types'
+import { cleanPages, cleanIds, shortenIds } from '@/lib/utils/clean.util'
+import { applySchedulePublish } from '@/lib/site/processors/schedule.processor'
+import type { SiteData } from '@/lib/site/site.types'
 
-export function applySchedulePublish(db: SiteData) {
-  db.allPages?.forEach(p => {
-    if (!isInRange(p.title, p.date)) {
-      p.status = 'Invisible'
-    }
-  })
+export function handleDataBeforeReturn(db: SiteData): SiteData {
+  applySchedulePublish(db)
+
+  db.categoryOptions = cleanIds(db.categoryOptions)
+  db.customMenu = cleanIds(db.customMenu)
+
+  db.allNavPages = cleanPages(db.allNavPages, db.tagOptions)
+  db.allPages = cleanPages(db.allPages, db.tagOptions)
+  db.latestPosts = cleanPages(db.latestPosts, db.tagOptions)
+
+  delete db.block
+  delete db.schema
+  delete db.rawMetadata
+  delete db.pageIds
+
+  return db
 }

6feb810e73c2ffb205604e63cdc77f367fc6f6d7
chenyun170 <104297453+chenyun170@users.noreply.github.com>
2026-05-18 10:22:48 +0800

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -8,15 +8,15 @@ export default async function handler(req, res) {
 
   // ✅ 换成 4w4.dpdns.org 的接口
   const client = new OpenAI({
-    apiKey: "",        // 改成 4w4.dpdns.org 要求的 Token
-    baseURL: "https://fufu.iqach.top/v1", // OpenAI 兼容接口标准路径
+    apiKey: "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",        // 改成 4w4.dpdns.org 要求的 Token
+    baseURL: "https://integrate.api.nvidia.com/v1", // OpenAI 兼容接口标准路径
   });
 
   try {
     const { message } = req.body;
 
     const stream = await client.chat.completions.create({
-      model: "mimo-v2-flash",  // ⚠️ 关键：改成文档里列出的模型名
+      model: "openai/gpt-oss-120b",  // ⚠️ 关键：改成文档里列出的模型名
       messages: [
         { role: "system", content: "你是一个乐于助人的 AI 助手。" },
         { role: "user", content: message }
